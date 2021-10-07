701947c6b11e8e25b4264f2d39286243cb8c42ca
Gabriel Löff Sá de Moraes <gabiloff7@gmail.com>
2021-10-07 15:52:10 -0300

    Conectando em outra api

--- src/main/java/br/com/misterjackpot/model/Bitcoin.java
+++ src/main/java/br/com/misterjackpot/model/Bitcoin.java
@@ -13,4 +13,35 @@ public class Bitcoin {
 
     private LocalDate data;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getPreço() {
+        return preço;
+    }
+
+    public void setPreço(Double preço) {
+        this.preço = preço;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
 }
