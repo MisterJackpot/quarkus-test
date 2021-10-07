73a268530866e5c627e4dc57f5ec44be77309c0e
Gabriel Löff Sá de Moraes <gabiloff7@gmail.com>
2021-10-07 15:23:35 -0300

    Client

---
+++ src/main/java/br/com/misterjackpot/model/Bitcoin.java
@@ -0,0 +1,16 @@
+package br.com.misterjackpot.model;
+
+
+import java.time.LocalDate;
+
+public class Bitcoin {
+
+    private Long id;
+
+    private Double preço;
+
+    private String tipo;
+
+    private LocalDate data;
+
+}
